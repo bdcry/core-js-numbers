040fd7b1fe20b4dff2b60e86f51aefafa69ac66b
Tim <tim@Macbook-Pro.local>
2025-10-07 23:29:24 +0300

    feat: add solution for the task roundToSmallestInteger

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -479,7 +479,7 @@ function getFloatOnString(str) {
  * '10', 8              => 8
  */
 function getIntegerOnString(str, base) {
-  return Number.parseInt(str, base) || Number.parseFloat(str);
+  return Number.parseInt(str, base);
 }
 
 /**
@@ -493,8 +493,8 @@ function getIntegerOnString(str, base) {
  * 3.5      => false
  * 2 ** 53  => false
  */
-function isSafeInteger(/* number */) {
-  throw new Error('Not implemented');
+function isSafeInteger(number) {
+  return Number.isSafeInteger(number);
 }
 
 /**
@@ -507,8 +507,8 @@ function isSafeInteger(/* number */) {
  * 5.9  => 5
  * -5.1 => -6
  */
-function roundToSmallestInteger(/* number */) {
-  throw new Error('Not implemented');
+function roundToSmallestInteger(number) {
+  return Math.floor(number);
 }
 
 /**
